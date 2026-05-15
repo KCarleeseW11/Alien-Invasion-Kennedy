0f47b1d0c5603b80a5f97e4caf5200f26b12a233
Kennedy Walker <kwalk94@student.cscc.edu>
2026-05-15 14:17:28 -0400

    Intial commit

---
+++ AlienInvasion_Kennedy/Ship.py
@@ -0,0 +1,11 @@
+""" python programming
+Alien Invasion Game
+I will be modifying an Alien Game
+Starter Code: Original Starter Rep from:
+https://github.com/RedBeard41/alien_Invasion_starter.git
+Date: May 14 2026 """
+
+import pygame
+class Ship :
+    def__init__(self, Screen):
+    self.image = pygame.image.load ("assets/image/ship2")
